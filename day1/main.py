b88bd2655fe51622eb033c5b5f214787f6a282fc
Dennis Christensen <dennis.f.christensen@gmail.com>
2021-12-01 08:49:27 +0100

    day 1 completed

---
+++ day1/main.py
@@ -0,0 +1,34 @@
+def read_indata():
+    with open("indata.txt") as file:
+        data = file.read()
+    return data
+
+def refactor_indata(indata):
+    indata = indata.split("\n")
+    for i in range(len(indata)):
+        indata[i] = int(indata[i])
+    return indata
+
+def calcA(reports):
+    higher = 0
+    for i in range(1, len(reports)):
+        if reports[i] > reports[i-1]:
+            higher += 1
+    print(higher)
+
+def calcB(reports):
+    higher = 0
+    for i in range(3, len(reports)):
+        if reports[i] + reports[i-1] + reports[i-2] > reports[i-1] + reports[i-2] + reports[i-3]:
+            higher += 1
+    print(higher)
+
+def main():
+    indata = read_indata()
+    indata = refactor_indata(indata)
+    calcA(indata)
+    calcB(indata)
+
+
+if __name__ == "__main__":
+    main()
