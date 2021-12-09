bdc79da252dbe5571125528eb3c0bc0c651a7540
Dennis Christensen <dennis.f.christensen@gmail.com>
2021-12-09 14:04:01 +0100

    day 9 complete

---
+++ day9/main.py
@@ -0,0 +1,100 @@
+from os import path
+
+
+def read_indata():
+    with open("indata.txt") as file:
+        data = file.read()
+    return data
+
+def refactor_indata(indata):
+    indata = [[int(x) for x in y] for y in indata.split("\n")]
+    return indata
+
+def calcA(indata):
+    risk_level = 0
+    for y in range(len(indata)):
+        for x in range(len(indata[0])):
+            neighbours = []
+            if x > 0:
+                neighbours.append([-1,0])
+            if x < len(indata[0]) - 1:
+                neighbours.append([1,0])
+            if y > 0:
+                neighbours.append([0,-1])
+            if y < len(indata) - 1:
+                neighbours.append([0,1])
+
+            lowest_point = True
+            for n in neighbours:
+                if indata[y + n[1]][x + n[0]] <= indata[y][x]:
+                    lowest_point = False
+                    break
+
+            if lowest_point:
+                risk_level += indata[y][x] + 1
+
+    print(risk_level)
+            
+
+def calcB(indata):
+    low_points = []
+    basins = []
+    for y in range(len(indata)):
+        for x in range(len(indata[0])):
+            neighbours = []
+            if x > 0:
+                neighbours.append([-1,0])
+            if x < len(indata[0]) - 1:
+                neighbours.append([1,0])
+            if y > 0:
+                neighbours.append([0,-1])
+            if y < len(indata) - 1:
+                neighbours.append([0,1])
+
+            lowest_point = True
+            for n in neighbours:
+                if indata[y + n[1]][x + n[0]] <= indata[y][x]:
+                    lowest_point = False
+                    break
+
+            if lowest_point:
+                low_points.append([x,y])
+
+    for low_point in low_points:
+        basins.append(len(pathfinding(low_point, indata, {})))
+    
+    basins = sorted(basins)
+    print(basins[-1] * basins[-2] * basins[-3])
+
+def pathfinding(point, indata, seen={}):
+    neighbours = []
+    if point[0] > 0:
+        neighbours.append([-1,0])
+    if point[0] < len(indata[0]) - 1:
+        neighbours.append([1,0])
+    if point[1] > 0:
+        neighbours.append([0,-1])
+    if point[1] < len(indata) - 1:
+        neighbours.append([0,1])
+
+    for n in neighbours:
+        cur_point = [point[0] + n[0],point[1] + n[1]]
+        if f"{cur_point[0]},{cur_point[1]}" in seen:
+            continue
+        elif indata[cur_point[1]][cur_point[0]] == 9:
+            continue
+        else:
+            seen[f"{cur_point[0]},{cur_point[1]}"] = True
+            pathfinding(cur_point, indata, seen)
+    
+    return seen
+
+
+def main():
+    indata = read_indata()
+    indata = refactor_indata(indata)
+    calcA(indata)
+    calcB(indata)
+
+if __name__ == "__main__":
+    main()
