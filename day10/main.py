6e6c92b0ebd594d504f4bf49d2fc937a0d6db585
Dennis Christensen <dennis.f.christensen@gmail.com>
2021-12-10 06:21:20 +0100

    day 10 complete

---
+++ day10/main.py
@@ -0,0 +1,93 @@
+def read_indata():
+    with open("indata.txt") as file:
+        data = file.read()
+    return data
+
+def refactor_indata(indata):
+    indata = indata.split("\n")
+    return indata
+
+def calcA(indata):
+    opening = {
+        "(": ")", 
+        "[": "]",
+        "{": "}",
+        "<": ">"
+    }
+    closing = {
+        ")": "(", 
+        "]": "[",
+        "}": "{",
+        ">": "<"
+    }
+    points = {
+        ")": 3, 
+        "]": 57,
+        "}": 1197,
+        ">": 25137
+    }
+    score = 0
+    for row in indata:
+        opened = []
+        for char in row:
+            if char in opening:
+                opened.append(char)
+            elif char in closing and opened[-1] == closing[char]:
+                opened.pop()
+            else:
+                score += points[char]
+                break
+
+    print(score)
+
+def calcB(indata):
+    opening = {
+        "(": ")", 
+        "[": "]",
+        "{": "}",
+        "<": ">"
+    }
+    closing = {
+        ")": "(", 
+        "]": "[",
+        "}": "{",
+        ">": "<"
+    }
+    points = {
+        ")": 1, 
+        "]": 2,
+        "}": 3,
+        ">": 4
+    }
+    scores = []
+    for row in indata:
+        opened = []
+        is_corrupt = False
+        for char in row:
+            if char in opening:
+                opened.append(char)
+            elif char in closing and opened[-1] == closing[char]:
+                opened.pop()
+            else:
+                is_corrupt = True
+                break
+        if is_corrupt:
+            continue
+        score = 0
+        for char in opened[::-1]:
+            score *= 5
+            score += points[opening[char]]
+        scores.append(score)
+    scores = sorted(scores)
+    print(scores[len(scores)//2])
+
+
+
+def main():
+    indata = read_indata()
+    indata = refactor_indata(indata)
+    calcA(indata)
+    calcB(indata)
+
+if __name__ == "__main__":
+    main()
