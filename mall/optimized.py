92111b05ab70531b3d90ee30d0181ac1e82fb5b1
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2021-12-25 22:35:25 +0100

    fixed template folder

--- mall/optimized.py
+++ mall/optimized.py
@@ -1,10 +1,3 @@
-from time import perf_counter
-
-def read_indata():
-    with open("indata.txt") as file:
-        data = file.read()
-    return data
-
 def refactor_indata(indata):
     return indata
 
@@ -13,39 +6,3 @@ def calc_a(indata):
 
 def calc_b(indata):
     pass
-
-def main():
-    total_start = perf_counter()
-    indata = read_indata()
-    refactor_start = perf_counter()
-    indata = refactor_indata(indata)
-    refactor_end = perf_counter()
-    part1_start = perf_counter()
-    a = calc_a(indata)
-    part1_end = perf_counter()
-    part2_start = perf_counter()
-    b = calc_b(indata)
-    part2_end = perf_counter()
-    total_end = perf_counter()
-    print(f"Refactoring time: {time_to_str(refactor_end - refactor_start)}")
-    print(f"Part 1 calc time: {time_to_str(part1_end - part1_start)}")
-    print(f"Part 2 calc time: {time_to_str(part2_end - part2_start)}")
-    print(f"Total time:       {time_to_str(total_end - total_start)}")
-    print(f"Answer part 1:    {a}")
-    print(f"Answer part 2:    {b}")
-
-def time_to_str(time):
-    suffixes = {
-        "s": 1,
-        "ms": 0.001,
-        "µs": 0.000001,
-        "ns": 0.000000001,
-    }
-    for suffix in suffixes:
-        if time > suffixes[suffix]:
-            return f"{(time/suffixes[suffix]):.2f}" + suffix
-    return f"{time}"
-
-
-if __name__ == "__main__":
-    main()
