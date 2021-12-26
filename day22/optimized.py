206903d7b75d0bb7c803e77fd0d8afaefb7f0453
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2021-12-26 20:57:54 +0100

    part 1 day 22 complete

--- day22/optimized.py
+++ day22/optimized.py
@@ -1,8 +1,34 @@
 def refactor_indata(indata):
-    return indata
+    operations = [row.split() for row in indata.split("\n")]
+    for i in range(len(operations)):
+        operations[i] = (operations[i][0], (*[(*[int(x) for x in bound[2:].split("..")],) for bound in operations[i][1].split(",")],))
+    return (*operations,)
 
-def calc_a(indata):
-    pass
 
-def calc_b(indata):
-    pass
+def calc_a(operations):
+    on_cubes = set()
+    for op, bounds in operations:
+        if op == "on":
+            func = on_cubes.add
+        elif op == "off":
+            func = on_cubes.discard
+        ranges = [range(max(bound[0], -50), min(bound[1], 50) + 1) for bound in bounds]
+        for x in ranges[0]:
+            for y in ranges[1]:
+                for z in ranges[2]:
+                    func((x,y,z))
+    return len(on_cubes)
+
+def calc_b(operations):
+    on_cubes = set()
+    for op, bounds in operations:
+        if op == "on":
+            func = on_cubes.add
+        elif op == "off":
+            func = on_cubes.discard
+        ranges = [range(bound[0], bound[1] + 1) for bound in bounds]
+        for x in ranges[0]:
+            for y in ranges[1]:
+                for z in ranges[2]:
+                    func((x,y,z))
+    return len(on_cubes)
