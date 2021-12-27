e104d2a7d1300e520a6e3c84d003fcd4a392bea3
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2021-12-27 23:26:57 +0100

    wip day 24

--- day24/optimized.py
+++ day24/optimized.py
@@ -44,26 +44,26 @@ def reset(alu, w=0, x=0, y=0, z=0):
 def calc_a(monad): 
     alu = {"w": 0, "x": 0, "y": 0, "z": 0}
     funcs = {"add": add, "mul": mul, "div": div, "mod": mod, "eql": eql}
-    ops = monad[0]
-    possible_outcomes = set()
-    for j in range(1, 10):
-        reset(alu)
-        alu[ops[0][0]] = j
-        for op, x, y in ops[1:]:
-            funcs[op](alu, x, y)
-        possible_outcomes.add((alu["w"], alu["x"], alu["y"], alu["z"], str(j)))
+    # ops = monad[0]
+    # possible_outcomes = set()
+    # for j in range(1, 10):
+    #     reset(alu)
+    #     alu[ops[0][0]] = j
+    #     for op, x, y in ops[1:]:
+    #         funcs[op](alu, x, y)
+    #     possible_outcomes.add((alu["w"], alu["x"], alu["y"], alu["z"], str(j)))
     
-    for ops in monad[1:]:
-        new_possible_outcomes = set()
-        print(len(possible_outcomes))
-        for j in range(1, 10):
-            for *vals, prev_num in possible_outcomes:
-                reset(alu, *vals)
-                alu[ops[0][0]] = j
-                for op, x, y in ops[1:]:
-                    funcs[op](alu, x, y)
-                new_possible_outcomes.add((alu["w"], alu["x"], alu["y"], alu["z"], j))
-        possible_outcomes = new_possible_outcomes
+    # for ops in monad[1:]:
+    #     new_possible_outcomes = set()
+    #     print(len(possible_outcomes))
+    #     for j in range(1, 10):
+    #         for *vals, prev_num in possible_outcomes:
+    #             reset(alu, *vals)
+    #             alu[ops[0][0]] = j
+    #             for op, x, y in ops[1:]:
+    #                 funcs[op](alu, x, y)
+    #             new_possible_outcomes.add((alu["w"], alu["x"], alu["y"], alu["z"], j))
+    #     possible_outcomes = new_possible_outcomes
 
 
 def calc_b(indata):
