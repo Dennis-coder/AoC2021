6a549fa7efd4e4b6d147c9bfc3a8c297da33d395
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2021-12-27 23:26:24 +0100

    optimized day 21

--- day21/optimized.py
+++ day21/optimized.py
@@ -1,65 +1,42 @@
+from functools import cache
+from itertools import cycle
+
 def refactor_indata(indata):
     starting_pos = (*[int(x.split(": ")[1]) for x in indata.split("\n")],)
     return starting_pos
 
 def calc_a(starting_pos):
     players = [[starting_pos[0], 0], [starting_pos[1], 0]]
-    dice = 1
+    dice = cycle(range(1, 101))
+    turn = cycle([0,1])
     i = 0
     while players[0][1] < 1000 and players[1][1] < 1000:
-        player = players[i%2]
-        movement = 0
-        for _ in range(3):
-            movement += dice
-            dice = dice % 100 + 1
+        player = players[next(turn)]
+        movement = next(dice) + next(dice) + next(dice)
         player[0] = (player[0] + movement - 1) % 10 + 1
         player[1] += player[0]
-        i += 1
-    return (players[0][1] if players[0][1] < 1000 else players[1][1]) * i*3
+        i += 3
+    (_, p1_score), (_, p2_score) = players
+    return min(p1_score, p2_score) * i
+
+
+rolls = {3:1, 4:3, 5:6, 6:7, 7:6, 8:3, 9:1}
+@cache
+def compute_wins(p1, p1_score, p2, p2_score):
+    p1_wins = p2_wins = 0
+    for roll, ct in rolls.items():
+        new_p1 = (p1 + roll - 1) % 10 + 1
+        new_p1_score = p1_score + new_p1
+        if new_p1_score >= 21:
+            p1_wins += ct
+        else:
+            tmp_p2_wins, tmp_p1_wins = compute_wins(p2, p2_score, new_p1, new_p1_score)
+            p1_wins += tmp_p1_wins * ct
+            p2_wins += tmp_p2_wins * ct
+
+    return p1_wins, p2_wins
 
 def calc_b(starting_pos):
-    universes = {
-        (*starting_pos, 0, 0, True): 1
-    }
-    player_1_wins = 0
-    player_2_wins = 0
-    i = 0
-    while universes:
-        new_universes = {}
-        for (p1_pos, p2_pos, p1_score, p2_score, p1_turn), n in universes.items():
-            if p1_turn:
-                new_states = [
-                    (((p1_pos + 2) % 10 + 1, p2_pos, p1_score + ((p1_pos + 2) % 10 + 1), p2_score, False), 1),
-                    (((p1_pos + 3) % 10 + 1, p2_pos, p1_score + ((p1_pos + 3) % 10 + 1), p2_score, False), 3),
-                    (((p1_pos + 4) % 10 + 1, p2_pos, p1_score + ((p1_pos + 4) % 10 + 1), p2_score, False), 6),
-                    (((p1_pos + 5) % 10 + 1, p2_pos, p1_score + ((p1_pos + 5) % 10 + 1), p2_score, False), 7),
-                    (((p1_pos + 6) % 10 + 1, p2_pos, p1_score + ((p1_pos + 6) % 10 + 1), p2_score, False), 6),
-                    (((p1_pos + 7) % 10 + 1, p2_pos, p1_score + ((p1_pos + 7) % 10 + 1), p2_score, False), 3),
-                    (((p1_pos + 8) % 10 + 1, p2_pos, p1_score + ((p1_pos + 8) % 10 + 1), p2_score, False), 1),
-                ]
-                for state, m in new_states:
-                    if state[2] >= 21:
-                        player_1_wins += n*m
-                    else:
-                        if state not in new_universes:
-                            new_universes[state] = 0
-                        new_universes[state] += n*m
-            else:
-                new_states = [
-                    ((p1_pos, (p2_pos + 2) % 10 + 1, p1_score, p2_score + ((p2_pos + 2) % 10 + 1), True), 1),
-                    ((p1_pos, (p2_pos + 3) % 10 + 1, p1_score, p2_score + ((p2_pos + 3) % 10 + 1), True), 3),
-                    ((p1_pos, (p2_pos + 4) % 10 + 1, p1_score, p2_score + ((p2_pos + 4) % 10 + 1), True), 6),
-                    ((p1_pos, (p2_pos + 5) % 10 + 1, p1_score, p2_score + ((p2_pos + 5) % 10 + 1), True), 7),
-                    ((p1_pos, (p2_pos + 6) % 10 + 1, p1_score, p2_score + ((p2_pos + 6) % 10 + 1), True), 6),
-                    ((p1_pos, (p2_pos + 7) % 10 + 1, p1_score, p2_score + ((p2_pos + 7) % 10 + 1), True), 3),
-                    ((p1_pos, (p2_pos + 8) % 10 + 1, p1_score, p2_score + ((p2_pos + 8) % 10 + 1), True), 1),
-                ]
-                for state, m in new_states:
-                    if state[3] >= 21:
-                        player_2_wins += n*m
-                    else:
-                        if state not in new_universes:
-                            new_universes[state] = 0
-                        new_universes[state] += n*m
-        universes = new_universes
-    return max((player_1_wins, player_2_wins))
+    p1, p2 = starting_pos
+    p1_wins, p2_wins = compute_wins(p1, 0, p2, 0)
+    return max(p1_wins, p2_wins)
