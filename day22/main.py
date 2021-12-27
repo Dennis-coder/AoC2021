c377dd8a394854cd5895fb54d83cae781d977bff
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2021-12-27 23:26:34 +0100

    completed day 22

--- day22/main.py
+++ day22/main.py
@@ -1,8 +1,92 @@
 def refactor_indata(indata):
-    return indata
+    operations = [row.split() for row in indata.split("\n")]
+    for i in range(len(operations)):
+        operations[i] = (operations[i][0], (*[(*[int(x) for x in bound[2:].split("..")],) for bound in operations[i][1].split(",")],))
+    return (*operations,)
 
-def calc_a(indata):
-    pass
 
-def calc_b(indata):
-    pass
+def calc_a(operations):
+    on_cubes = set()
+    for op, bounds in operations:
+        if op == "on":
+            func = on_cubes.add
+        elif op == "off":
+            func = on_cubes.discard
+        ranges = [range(max(bound[0], -50), min(bound[1], 50) + 1) for bound in bounds]
+        for x in ranges[0]:
+            for y in ranges[1]:
+                for z in ranges[2]:
+                    func((x,y,z))
+    return len(on_cubes)
+
+class Cube:
+
+    def __init__(self, x1, x2, y1, y2, z1, z2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.z1 = z1
+        self.z2 = z2
+    
+    def __repr__(self):
+        return f"{self.x1=}, {self.x2=}, {self.y1=}, {self.y2=}, {self.z1=}, {self.z2=}"
+
+    def size(self):
+        return (
+            (self.x2 - self.x1) *
+            (self.y2 - self.y1) *
+            (self.z2 - self.z1)
+        )
+    
+    def intersects(self, other):
+        return (
+            self.x1 < other.x2 and
+            self.x2 > other.x1 and
+            self.y1 < other.y2 and
+            self.y2 > other.y1 and
+            self.z1 < other.z2 and
+            self.z2 > other.z1
+        )
+
+    def contains(self, other):
+        return (
+            self.x1 <= other.x1 and
+            self.x2 >= other.x2 and
+            self.y1 <= other.y1 and
+            self.y2 >= other.y2 and
+            self.z1 <= other.z1 and
+            self.z2 >= other.z2
+        )
+    
+    def subtract(self, other):
+        if not self.intersects(other):
+            return [self]
+        elif other.contains(self):
+            return []
+
+        xs = sorted((self.x1, self.x2, other.x1, other.x2))
+        ys = sorted((self.y1, self.y2, other.y1, other.y2))
+        zs = sorted((self.z1, self.z2, other.z1, other.z2))
+
+        ret = []
+        for x1, x2 in zip(xs, xs[1:]):
+            for y1, y2 in zip(ys, ys[1:]):
+                for z1, z2 in zip(zs, zs[1:]):
+                    cube = Cube(x1,x2,y1,y2,z1,z2)
+                    if self.contains(cube) and not cube.intersects(other):
+                        ret.append(cube)
+        return ret
+
+def calc_b(operations):
+    reboots = [[op[0] == "on", Cube(*sum([[x + i for i, x in enumerate(bound)] for bound in op[1]], []))] for op in operations]
+    cubes = []
+    for on, step_cube in reboots:
+        cubes = [
+            part
+            for cube in cubes
+            for part in cube.subtract(step_cube)
+        ]
+        if on:
+            cubes.append(step_cube)
+    return sum(cube.size() for cube in cubes)
--- day22/optimized.py
+++ day22/optimized.py
@@ -1,47 +1,102 @@
-from copy import deepcopy
-
 def refactor_indata(indata):
-    operations = [row.split() for row in indata.split("\n")]
-    for i in range(len(operations)):
-        operations[i] = (operations[i][0], (*[(*[int(x) for x in bound[2:].split("..")],) for bound in operations[i][1].split(",")],))
-    return (*operations,)
+    # on x=-17..30,y=-5..41,z=-33..14
+    operations = []
+    for line in indata.splitlines():
+        op, bounds = line.split()
+        x,y,z = bounds.split(",")
+        x1, x2 = [int(a) for a in x[2:].split("..")]
+        y1, y2 = [int(a) for a in y[2:].split("..")]
+        z1, z2 = [int(a) for a in z[2:].split("..")]
+        operations.append((op == "on", Cube(x1, x2 + 1, y1, y2 + 1 , z1, z2 + 1)))
+    return operations
+    
+class Cube:
 
+    def __init__(self, x1, x2, y1, y2, z1, z2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.z1 = z1
+        self.z2 = z2
+    
+    def __repr__(self):
+        return f"{self.x1=}, {self.x2=}, {self.y1=}, {self.y2=}, {self.z1=}, {self.z2=}"
 
+    def size(self):
+        return (
+            (self.x2 - self.x1) *
+            (self.y2 - self.y1) *
+            (self.z2 - self.z1)
+        )
+    
+    def intersects(self, other):
+        return (
+            self.x1 < other.x2 and
+            self.x2 > other.x1 and
+            self.y1 < other.y2 and
+            self.y2 > other.y1 and
+            self.z1 < other.z2 and
+            self.z2 > other.z1
+        )
+
+    def contains(self, other):
+        return (
+            self.x1 <= other.x1 and
+            self.x2 >= other.x2 and
+            self.y1 <= other.y1 and
+            self.y2 >= other.y2 and
+            self.z1 <= other.z1 and
+            self.z2 >= other.z2
+        )
+    
+    def subtract(self, other):
+        if not self.intersects(other):
+            return [self]
+        elif other.contains(self):
+            return []
+
+        xs = sorted((self.x1, self.x2, other.x1, other.x2))
+        ys = sorted((self.y1, self.y2, other.y1, other.y2))
+        zs = sorted((self.z1, self.z2, other.z1, other.z2))
+
+        ret = []
+        for x1, x2 in zip(xs, xs[1:]):
+            for y1, y2 in zip(ys, ys[1:]):
+                for z1, z2 in zip(zs, zs[1:]):
+                    cube = Cube(x1,x2,y1,y2,z1,z2)
+                    if self.contains(cube) and not cube.intersects(other):
+                        ret.append(cube)
+        return ret
+    
+    def overlapping_cube(self, other):
+        ((x1,x2),(y1,y2),(z1,z2)) = ((max(self.x1,other.x1),min(self.x2,other.x2)),(max(self.y1,other.y1),min(self.y2,other.y2)),(max(self.z1,other.z1),min(self.z2,other.z2)))
+        return Cube(x1,x2,y1,y2,z1,z2)
+        
 def calc_a(operations):
-    on_cubes = set()
-    for op, bounds in operations:
-        if op == "on":
-            func = on_cubes.add
-        elif op == "off":
-            func = on_cubes.discard
-        ranges = [range(max(bound[0], -50), min(bound[1], 50) + 1) for bound in bounds]
-        for x in ranges[0]:
-            for y in ranges[1]:
-                for z in ranges[2]:
-                    func((x,y,z))
-    return len(on_cubes)
+    cubes = []
+    bound = Cube(-50,51,-50,51,-50,51)
+    for on, step_cube in operations:
+        if not bound.intersects(step_cube):
+            continue
+        step_cube = bound.overlapping_cube(step_cube)
+        cubes = [
+            part
+            for cube in cubes
+            for part in cube.subtract(step_cube)
+        ]
+        if on:
+            cubes.append(step_cube)
+    return sum(cube.size() for cube in cubes)
 
 def calc_b(operations):
-    total_volume = 0
-    volumes = []
-    for op, ((x1,x2),(y1,y2),(z1,z2)) in operations:
-        for op2, ((x3,x4),(y3,y4),(z3,z4)) in deepcopy(volumes):
-            ((x5,x6),(y5,y6),(z5,z6)) = ((max(x1,x3),min(x2,x4)),(max(y1,y3),min(y2,y4)),(max(z1,z3),min(z2,z4)))
-            if x6-x5 == 0 or y6-y5 == 0 or z6-z5 == 0:
-                continue
-            if op == "on" and op2 == "on":
-                total_volume -= (x6-x5+1) * (y6-y5+1) * (z6-z5+1)
-                volumes.append(("off", ((x5,x6),(y5,y6),(z5,z6))))
-            elif op == "on" and op2 == "off":
-                total_volume += (x6-x5+1) * (y6-y5+1) * (z6-z5+1)
-                volumes.append(("on", ((x5,x6),(y5,y6),(z5,z6))))
-            elif op == "off" and op2 == "on":
-                total_volume -= (x6-x5+1) * (y6-y5+1) * (z6-z5+1)
-                volumes.append(("off", ((x5,x6),(y5,y6),(z5,z6))))
-            elif op == "off" and op2 == "off":
-                total_volume += (x6-x5+1) * (y6-y5+1) * (z6-z5+1)
-                volumes.append(("on", ((x5,x6),(y5,y6),(z5,z6))))
-        if op == "on":
-            total_volume += (x2-x1+1) * (y2-y1+1) * (z2-z1+1)
-            volumes.append((op, ((x1,x2),(y1,y2),(z1,z2))))
-    return total_volume
+    cubes = []
+    for on, step_cube in operations:
+        cubes = [
+            part
+            for cube in cubes
+            for part in cube.subtract(step_cube)
+        ]
+        if on:
+            cubes.append(step_cube)
+    return sum(cube.size() for cube in cubes)
