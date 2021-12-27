aaf523f8519b07258308a68c48229abd69210116
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2021-12-27 23:26:13 +0100

    completed day 20

--- day20/main.py
+++ day20/main.py
@@ -1,8 +1,45 @@
+from collections import defaultdict
+
 def refactor_indata(indata):
-    return indata
+    algorithm, image = indata.split("\n\n")
+    image = (*image.split(),)
+    return (algorithm, image)
+
+def get_index(x,y, pixels):
+    offsets = [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
+    index = 0
+    for dx,dy in offsets:
+        index <<= 1
+        index += pixels[(x+dx,y+dy)]
+    return index
+
+def calc(indata, iterations):
+    algorithm, image = indata
+    pixels = defaultdict(lambda: 0)
+    for y, row in enumerate(image):
+        for x, char in enumerate(row):
+            pixels[(x,y)] = 0 if char == "." else 1
+    for i in range(iterations):
+        minx = min(x for x, _ in pixels)
+        maxx = max(x for x, _ in pixels)
+        miny = min(y for _, y in pixels)
+        maxy = max(y for _, y in pixels)
+
+        if algorithm[0] == "#" and algorithm[-1] == "." and i % 2 == 0:
+            new_pixels = defaultdict(lambda: 1)
+        else:
+            new_pixels = defaultdict(lambda: 0)
+
+        for y in range(miny - 1, maxy + 2):
+            for x in range(minx - 1, maxx + 2):
+                index = get_index(x, y, pixels)
+                new_pixels[(x,y)] = 0 if algorithm[index] == "." else 1
+
+        pixels = new_pixels
+    return sum(pixels.values())
 
 def calc_a(indata):
-    pass
+    return calc(indata, 2)
 
 def calc_b(indata):
-    pass
+    return calc(indata, 50)
--- day20/optimized.py
+++ day20/optimized.py
@@ -1,41 +1,41 @@
+from collections import defaultdict
+
 def refactor_indata(indata):
     algorithm, image = indata.split("\n\n")
     image = (*image.split(),)
     return (algorithm, image)
 
-def get_index(x,y, lit_pixels, size, rest):
-    offsets = [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
-    binary = ""
-    for dx,dy in offsets:
-        binary += "1" if (x+dx,y+dy) in lit_pixels else "0" if (size[0][0] <= x+dx <= size[0][1] and size[1][0] <= y+dy <= size[1][1]) or rest == "." else "1"
-    return int(binary, 2)
-
-def calc_a(indata):
+def calc(indata, iterations):
     algorithm, image = indata
-    lit_pixels = set()
+    pixels = defaultdict(lambda: 0)
+    offsets = [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]
     for y, row in enumerate(image):
         for x, char in enumerate(row):
-            if char == "#":
-                lit_pixels.add((x,y))
-    size = ((0,len(image[0]) - 1),(0,len(image) - 1))
-    rest = "."
-    for _ in range(2):
-        new_lit_pixels = set()
-        (x_low, x_high),(y_low, y_high) = size
-        for y in range(y_low - 1, y_high + 2):
-            for x in range(x_low - 1, x_high + 2):
-                index = get_index(x, y, lit_pixels, size, rest)
-                char = algorithm[index]
-                if char == "#":
-                    new_lit_pixels.add((x,y))
-                    x_low = min((x_low, x))
-                    x_high = max((x_high, x))
-                    y_low = min((y_low, y))
-                    y_high = max((y_high, y))
-        size = ((x_low, x_high),(y_low, y_high))
-        lit_pixels = new_lit_pixels
-        rest = "." if algorithm[0 if rest == "." else -1] == "." else "#"
-    return len(lit_pixels)
+            pixels[(x,y)] = 0 if char == "." else 1
+    for i in range(iterations):
+        minx = min(x for x, _ in pixels)
+        maxx = max(x for x, _ in pixels)
+        miny = min(y for _, y in pixels)
+        maxy = max(y for _, y in pixels)
+
+        if algorithm[0] == "#" and algorithm[-1] == "." and i % 2 == 0:
+            new_pixels = defaultdict(lambda: 1)
+        else:
+            new_pixels = defaultdict(lambda: 0)
+
+        for y in range(miny - 1, maxy + 2):
+            for x in range(minx - 1, maxx + 2):
+                index = 0
+                for dx,dy in offsets:
+                    index <<= 1
+                    index += pixels[(x+dx,y+dy)]
+                new_pixels[(x,y)] = 0 if algorithm[index] == "." else 1
+
+        pixels = new_pixels
+    return sum(pixels.values())
+
+def calc_a(indata):
+    return calc(indata, 2)
 
 def calc_b(indata):
-    algorithm, image = indata
+    return calc(indata, 50)
